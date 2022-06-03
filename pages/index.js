dd79e55abaa42acc12abed4abadb41d232e1ee2d
Trinath Anantham <trinath.anantham@gmail.com>
2022-06-03 11:49:59 +0530

    Added material ui, sass support

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 export default function index() {
-  return <div>index</div>;
+  return (
+    <Box
+      className="paper-content"
+      component="form"
+      sx={{
+        "& > :not(style)": { m: 1, width: "25ch" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      <TextField id="filled-basic" label="Filled" variant="filled" />
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+    </Box>
+  );
 }
